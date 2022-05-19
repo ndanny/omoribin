3058b204ce44e74e52dacf93c386671f2453fb1f
Danny Nguyen <danfordnguyen@gmail.com>
2022-05-19 15:14:11 -0400

    Mounts the index path to rocket

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,24 @@ use rocket::{Rocket, Build};
 #[macro_use]
 extern crate rocket;
 
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+    
+        POST /
+
+            accepts raw data from the request body and responds
+            with a url of the paste
+
+        GET /<id>
+
+            retrieves content for paste <id>
+    "
+}
+
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
+        .mount("/", routes![index])
 }
