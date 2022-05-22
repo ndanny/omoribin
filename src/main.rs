ab44de468c631ad5fda0ade2373bcd2d8c79e72d
Danny Nguyen <danfordnguyen@gmail.com>
2022-05-22 02:27:05 -0400

    Impl show_paste

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use constants::ID_SIZE;
 use paste_id::PasteId;
 use rocket::data::ToByteUnit;
 use rocket::http::uri::Absolute;
-use rocket::response::Redirect;
+// use rocket::response::Redirect;
 use rocket::tokio::fs::{self, File};
 use rocket::{Data, Request};
 use rocket_dyn_templates::{context, Template};
@@ -28,6 +28,35 @@ fn not_found(req: &Request<'_>) -> Template {
         },
     )
 }
+
+#[get("/show/<id>")]
+async fn show_paste(id: PasteId<'_>) -> Template {
+    let mut content = String::new();
+    let mut error = false;
+
+    match fs::read_to_string(id.file_path()).await {
+        Ok(p) => content.push_str(&p),
+        Err(_) => error = true
+    }
+
+    Template::render("show", context! {
+            paste: content,
+            error: error
+        },
+    )
+}
+
+// #[post("/create", data = "<paste>")]
+// async fn create_paste(paste: Data<'_>) -> Redirect {
+//     let id = PasteId::new(ID_SIZE);
+//     let host: Absolute<'static> = uri!("http://localhost:8000");
+//     paste
+//         .open(128.kibibytes())
+//         .into_file(id.file_path())
+//         .await?;
+
+//     Ok(uri!(host, read_api(id)).to_string())
+// }
 
 #[post("/v1/create", data = "<paste>")]
 async fn create_api(paste: Data<'_>) -> std::io::Result<String> {
@@ -65,8 +94,11 @@ async fn delete_api(id: PasteId<'_>) -> Option<()> {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
-        .mount("/api", routes![create_api, read_api, update_api, delete_api])
+        .mount("/", routes![index, show_paste])
+        .mount(
+            "/api",
+            routes![create_api, read_api, update_api, delete_api],
+        )
         .register("/", catchers![not_found])
         .attach(Template::fairing())
 }
