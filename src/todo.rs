f956f60eb03f49c44fe57d3390a24ecd1a9d8551
Danny Nguyen <danfordnguyen@gmail.com>
2022-05-28 18:45:40 -0400

    Modify CSS for better paste display

--- src/todo.rs
+++ src/todo.rs
@@ -3,7 +3,6 @@
 /// - Manage partial uploads over the 128KB upload limit with a 206 status code.
 /// - Return a unique token after each upload to be used during delete requests.
 /// - Dispatch a thread before launching Rocket that periodically cleans up old pastes.
-/// - Create CSS for better paste display
 ///
 /// Finished list
 /// - Create Omoribin.
@@ -16,5 +15,6 @@
 /// - Add a web form for users to manually input new pastes.
 /// - Separate web API from service API.
 /// - Integrate Rocket form parsing for user paste requests on the web.
+/// - Create CSS for better paste display
 
 struct _IGNORE {}
